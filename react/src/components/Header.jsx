f788238f3035684dd01017d588a3a03688ef5ab0
Bidesh <bideshmalofficial@gmail.com>
2024-08-21 16:46:31 +0530

    fixed errors half

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -3,19 +3,19 @@ import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 function Header() {
   return (
-    <header className='bg-slate-200 shadow nd'>
+    <header className='bg-slate-200 shadow-md'>
     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to={'/'}>
-        <h1 className='font-bold text-sm: text-xl flex-wrap'>
-            <span > Online Judge</span>
-            <span> by Bidesh</span>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'> Online Judge</span>
+            <span className='text-slate-700'> by Bidesh</span>
         </h1>
         </Link>
 
         
     
         <form className='text-black-700 bg-slate-100 p-3 rounded-lg flex items-center' >
-            <input type="text" placeholder='Search Problems' className='bg-transparent focus outline-none w-24 sm:w-64'/>
+            <input type="text" placeholder='Search Problems' className='bg-transparent focus:outline-none w-24 sm:w-64 md:w-40'/>
             <FaSearch className='text-slate-600'/>
         </form>
         <ul className='flex gap-4'>
@@ -23,11 +23,17 @@ function Header() {
             <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
         </Link>
         <Link to='/sign-in'>
-            <li className='hover:underline'>Sign In</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Sign In</li>
         </Link>
         <Link to='/about'>
-            <li className='hidden sm:inline hover:underline'>About</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
         </Link>
+        {/* <Link to='/sign-up'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Sign Up</li>
+        </Link>
+        <Link to='/profile'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Profile</li>
+        </Link> */}
             
             
         </ul>
